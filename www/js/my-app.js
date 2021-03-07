d3c7a84453ded10de64b53a6c65456bd1d5440af
MagaliVivas <vivasmagali.c@gmail.com>
2021-03-07 15:53:25 -0600

    Arreglo de registro

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -18,10 +18,6 @@ var app = new Framework7({
     routes: [
       { path: '/about/', url: 'about.html', },
       { path: '/registro/', url: 'registro.html', },
-      { path: '/ingresar/', url: 'ingresar.html', },
-      { patch: '/inicio/', url:'inicio.html'},
-      { path: '/busqueda/', url: 'busqueda.html', },
-      { path: '/menu/', url: 'menu.html', },
     ]
     // ... other parameters
   });
@@ -39,56 +35,53 @@ $$(document).on('page:init', '.page[data-name="index"]', function (e) {
         mainView.router.navigate('/registro/');
               
     });
-    $$("#Ir-Ingresar").on('click', function() {
-        console.log('click en ingresar');
-        mainView.router.navigate('/ingresar/');
-              
-    });
 })
 
 // Option 2. Using live 'page:init' event handlers for each page
-$$(document).on('page:init', '.page[data-name="ingresar"]', function (e) {
+$$(document).on('page:init', '.page[data-name="about"]', function (e) {
     // Do something here when page with data-name="about" attribute loaded and initialized
-
+    console.log(e);
+    alert('Hello');
 })
 
 
 $$(document).on('page:init', '.page[data-name="registro"]', function (e) {
-var varEmail="";
-var varPassword="";
-console.log("Llegue funcion de registro");
-    $$("BtnRegistrarse").on('click', function() {
-        if($$("#R-nombre").val() != "" || $$("#R-email").val() != "" || $$("#R-password").val() != ""){
-            varEmail= $$("#email").val();
-            varPassword= $$("#password").val();
-            CrearUsuario(varEmail,varPassword);
-            console.log("Entre al if");
-            console.log("El mail " + varEmail +" y la contraseña " + varPassword);
-        }else{
-            console.log("entre al else");
+var Email="";
+var Password="";
+    $$("#BtnRegistrarse").on('click', function() {
+        console.log("llegue a funcion registrarse");
+        /*if(($$("#R-nombre").val() != "" )&& ($$("#R-email").val() != "") && ($$("#R-password").val() != "")){
+            console.log("llegue if");*/
+            Email= $$("#R-email").val();
+            Password= $$("#R-password").val();
+            console.log("La contraseña es " + Password + "Y el mail es " + Email);
+            CrearUsuario(Email,Password);
+        /*}else{
+            console.log("llegue else");
             app.dialog.alert("Completa todo los campos","Atención");
-        };
+        };*/
     });
 })
 
 
 
 
-
-
-function CrearUsuario() {
-    console.log("Crear usuario");
+function CrearUsuario(varEmail,varPassword) {
+    console.log("Hola");
     firebase.auth().createUserWithEmailAndPassword(varEmail, varPassword)
         .catch(function(error) {
             // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
             if (errorCode == 'auth/weak-password') {
-                alert('Clave muy débil.');
+                app.dialog.alert('Clave muy débil.');
             } else {
-                alert(errorMessage);
+                if (errorCode == 'auth/email-already-in-use') {
+                    app.dialog.alert("La dirección de correo electrónico ya está siendo utilizada por otra cuenta.", "Atencion");
+                }else {
+                    app.dialog.alert("Completa todos los campos","Atención");
+                }
             }
             console.log(error);
         });
-        alert("q paso?");
 }
