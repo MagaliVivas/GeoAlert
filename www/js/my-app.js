2f7ad86871385aead0fc6759d40e70d432ec2421
MagaliVivas <vivasmagali.c@gmail.com>
2021-03-13 21:13:17 -0600

    Errores

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -27,6 +27,11 @@ var app = new Framework7({
 
 var mainView = app.views.create('.view-main');
 var NombreUsuario = "";
+var EmailUsuario ="";
+var PasswordUsuario="";
+var map, platform;
+var pos, latitud, longitud;
+
 
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
@@ -34,13 +39,30 @@ $$(document).on('deviceready', function() {
 });
 
 $$(document).on('page:init', '.page[data-name="index"]', function (e) {
+    console.log("Vista index");
+    
+ 
     $$("#BtnIngresar").on('click', function() {
-    if(($$("#I-email").val() == "" )|| ($$("#I-password").val() == "")){
-        app.dialog.alert("Complete todos los campos", "Atención");
-    }  else {
-        mainView.router.navigate('/inicio/');
-    }
-    })
+        if(($$("#I-email").val() != "" )&& ($$("#I-password").val() != "")) {
+            EmailUsuario = $$("#I-email").val();
+            PasswordUsuario = $$("#I-password").val();
+            /*console.log("El email es: " + EmailUsuario + "Y la contraseña es " + PasswordUsuario);*/
+                firebase.auth().signInWithEmailAndPassword(EmailUsuario, PasswordUsuario)
+                    .then((user) => {
+                        mainView.router.navigate('/inicio/');
+                      })
+                      .catch((error) => {
+                        var errorCode = error.code;
+                        var errorMessage = error.message;
+                        if (errorCode= 'auth/argument-error') {
+                            console.log("Entre al error de mail")
+                            app.dialog.alert("E-mail o contraseña incorrecta");
+                        }
+                });
+        } else {
+            app.dialog.alert("Complete todos los campos", "Atención");
+        } 
+        })
     $$('.login-screen').on('loginscreen:open', function (e) {
       console.log('Login screen open')
     });
@@ -59,12 +81,13 @@ $$(document).on('page:init', '.page[data-name="index"]', function (e) {
         mainView.router.navigate('/registro/');
               
     });
+
 })
 
 
-$$(document).on('page:init', '.page[data-name="ingresar"]', function (e) {
+$$(document).on('page:init', '.page[data-name="ayuda"]', function (e) {
     // Do something here when page with data-name="about" attribute loaded and initialized
-    
+   $$("#HolaAyuda").val("Hola NOMBRE, ¿En qué puedo ayudarte?" ); 
 })
 // Option 2. Using live 'page:init' event handlers for each page
 $$(document).on('page:init', '.page[data-name="about"]', function (e) {
@@ -76,22 +99,83 @@ $$(document).on('page:init', '.page[data-name="about"]', function (e) {
 
 
 $$(document).on('page:init', '.page[data-name="crear-alarma"]', function (e) {
-    // Do something here when page with data-name="about" attribute loaded and initialized
- $$("#BtnExtenderGrupo").on('click', function() {
-    $$("#GrupoExt").removeClass("invisible").addClass("visible");
-    console.log("entre a remove class de grupo");
- })
- $$("#BtnExtenderZona").on('click', function() {
-    $$("#ZonaExt").removeClass("invisible").addClass("visible");
-    console.log("Entre a remove class de zona");
- })
+    console.log("Vista crear-alarma");
+
+platform = new H.service.Platform({
+    'apikey': 'hRXfUDJNgcg6ppmlxyXIhYk6Qaolj9bM5CzsJ8B_qi0'
+   });
+        var defaultLayers = platform.createDefaultLayers();
+    latitud = -34.5;
+    longitud = -53.6;
+    map = new H.Map( document.getElementById('mapContainer'), defaultLayers.vector.normal.map,
+    {
+          zoom: 16,
+          center: { lat: latitud, lng: longitud },
+          volatility: true,
+          pixelRatio: window.devicePixelRatio || 1
+          });
+    coords = {lat: latitud, lng: longitud};
+    window.addEventListener('resize', () => map.getViewPort().resize());
+ 
+// Centramos el mapa en las coordenadas de la marca
+map.setCenter(coords);
+ 
+// Creamos una instancia de eventos de mapa:
+var mapEvents = new H.mapevents.MapEvents(map);
+ 
+// Creamos un controlador de eventos para el mapa:
+map.addEventListener('tap', function(evt) {
+  // console.log(evt.type, evt.currentPointer.type);
+});
+ 
+// Evento arrastrar
+var behavior = new H.mapevents.Behavior(mapEvents);
+ 
+// Activamos la Interfaz de usuario 
+var ui = H.ui.UI.createDefault(map, defaultLayers, 'es-ES')
+ 
+// Configuramos posicion de las opciones
+var mapSettings = ui.getControl('mapsettings');
+var zoom = ui.getControl('zoom');
+var scalebar = ui.getControl('scalebar');
+ 
+mapSettings.setAlignment('top-right');
+zoom.setAlignment('right-bottom');
+scalebar.setAlignment('bottom-left');
+
+var ExtGrupo = false;
+var ExtZona = false;
+$$("#BtnExtenderGrupo").on('click', function() {
+    if (ExtGrupo = false) {
+        $$("#GrupoExt").removeClass("invisible").addClass("visible");
+        ExtGrupo = true;
+        console.log("deberia mostrar zona de grupos");
+    } else {
+        $$("#GrupoExt").removeClass("visible").addClass("invisible");
+        ExtGrupo = false;
+        console.log("deberia borrar zona de grupos");
+    }
 })
+ $$("#BtnExtenderZona").on('click', function() {
+    if(ExtZona = false) {
+    $$("#ZonaExt").removeClass("invisible").addClass("visible");
+    console.log("deberia mostrar zonas");
+    ExtZona = true;
+    } else {
+        $$("#ZonaExt").removeClass("visible").addClass("invisible");
+        console.log("deberia borrar zonas");
+        ExtZona = false; 
+    }
+})
+})
+
 
 
 
 
 $$(document).on('page:init', '.page[data-name="inicio"]', function (e) {
     // Do something here when page with data-name="about" attribute loaded and initialized
+
     $$("#CrearAlarma").on('click', function() {
         mainView.router.navigate('/crear-alarma/');
     })
@@ -101,7 +185,19 @@ $$(document).on('page:init', '.page[data-name="inicio"]', function (e) {
       });
         mainView.router.navigate('/ayuda/');
     })
-    $$("#HolaAyuda").val("Hola NOMBRE, ¿En qué puedo ayudarte?" );
+    $$("#Enviadas").on('click', function() {
+        $$("#Recibidas").removeClass("ON");
+        $$("#TxtRecibidas").attr('src','recibidas.png');
+        $$("#Enviadas").addClass("ON");
+        $$("#TxtEnviadas").attr('src','enviadasON.png');
+        console.log("Remove class recibidas");
+    })
+    $$("#Recibidas").on('click', function() {
+        $$("#Enviadas").removeClass("ON");
+        $$("#TxtEnviadas").attr('src','enviadas.png');
+        $$("#Recibidas").addClass("ON");
+        $$("#TxtRecibidas").attr('src','recibidasON.png');
+    })
 })
 
 $$(document).on('page:init', '.page[data-name="registro"]', function (e) {
@@ -109,11 +205,16 @@ var Email="";
 var Password="";
     $$("#BtnRegistrarse").on('click', function() {
         console.log("llegue a funcion registrarse");
+        if ($$("#R-password1").val() != $$("#R-password2").val()) {
+            app.dialog.alert("Las contraseñas no coinciden", "Atención");
+        } else {
             Email= $$("#R-email").val();
-            Password= $$("#R-password").val();
+            Password= $$("#R-password1").val();
             console.log("La contraseña es " + Password + "Y el mail es " + Email);
             CrearUsuario(Email,Password);
-        NombreUsuario = $$("#I-email").val(); 
+        NombreUsuario = $$("#I-email").val();
+        console.log("NombreUsuario es" + NombreUsuario);
+        } 
     });
 })
 
@@ -126,6 +227,20 @@ function CrearUsuario(varEmail,varPassword) {
         .then(function(){
              if (registro == true) {
                 console.log("entré al if de crear usuario");
+                  firebase.auth().onAuthStateChanged(function(user) {
+                    console.log("user.displayName: " + user.displayName);
+                    user
+                    .updateProfile({ 
+                      displayName: NombreUsuario,
+                    })
+                    .then(function(){
+                      console.log("user.displayName: " + user.displayName);
+                      user.sendEmailVerification();
+                    });
+                  });
+
+                  app.dialog.alert("Verifique su email. Compruebe su casilla de correos","Atención");
+
                 mainView.router.navigate('/inicio/');
             }
         console.log("el valor de la variable verdadero booleana es " + registro);
